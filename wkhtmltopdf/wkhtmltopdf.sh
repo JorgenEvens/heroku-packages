3cb17217b8e3417fad58c55d378dd736c8f95752
Wim Mostmans <wim@sitebase.be>
2013-08-03 14:58:43 +0200

    download wkhtmltopdf package from S3 because github doesn't seem to allow downloading files from raw url that are bigger then 10MB

--- wkhtmltopdf/wkhtmltopdf.sh
+++ wkhtmltopdf/wkhtmltopdf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 WKHTMLTOPDF_VERSION="0.10.0.rc2"
-WKHTMLTOPDF_HOST="https://raw.github.com/Sitebase/heroku-packages/master/wkhtmltopdf"
+WKHTMLTOPDF_HOST="http://bubobox.deploy.s3.amazonaws.com/heroku/wkhtmltopdf"
 
 wkhtmltopdf_compile() {	
 	BINARIES="${CACHE_DIR}/wkhtmltopdf-${WKHTMLTOPDF_VERSION}.tar.gz"
