6c3697a3217ea5a00a453a384292961b9446e576
Jorgen Evens <jorgen@evens.eu>
2014-02-04 10:43:51 +0100

    Added PHP5 Redis extensions thanks to @Sitebase

---
+++ php5-fpm/php5-fpm-redis.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z "$PHP5_EXT_HELPER" ]; then
+	PHP5_EXT_HELPER="true"
+	download "${INSTALLER_DIR}/extension-helper" "${CACHE_DIR}/php5-extension-helper.sh"
+	. "${CACHE_DIR}/php5-extension-helper.sh"
+fi
+
+dependency_require "php5-fpm"
+php5ext_compile "redis"
